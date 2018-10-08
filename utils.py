6d051c035821ecf4518e3dc34e8ed02ca914bf53
larryhou <larryhou@foxmail.com>
2018-10-08 17:03:25 +0800

    update

--- utils.py
+++ utils.py
@@ -6,3 +6,4 @@ def backup(file_path:str):
     suffix = time.strftime('_%Y-%m-%d_%H%M%S', time.localtime())
     backup_file_path = re.sub(r'(\.[^.]+)$', r'{}\g<1>'.format(suffix), file_path)
     shutil.copy(file_path, backup_file_path)
+
